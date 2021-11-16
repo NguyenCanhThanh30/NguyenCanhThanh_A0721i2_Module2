cd38ab62cb6549b5a3715464c887681cce8fb53f
NguyenCanhThanh30 <83226544+NguyenCanhThanh30@users.noreply.github.com>
2021-11-16 17:04:31 +0700

    1234

---
+++ 12_java_collection_framework/bai_tap/luyen_tap_su_dung_arraylist_linkedlist/Main.java
@@ -0,0 +1,30 @@
+package bai12_java_collection_framework.bai_tap.luyen_tap_su_dung_arraylist_linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.setId(1);
+        product.setName("ZorinOS");
+        product.setPrice(40);
+
+        Product product1 = new Product();
+        product1.setId(2);
+        product1.setName("Ubuntu");
+        product1.setPrice(100);
+
+        Product product2 = new Product();
+        product2.setId(3);
+        product2.setName("Deepin");
+        product2.setPrice(10);
+
+        ProductManager productManager = new ProductManager();
+        productManager.addProduct(product);
+        productManager.addProduct(product1);
+        productManager.addProduct(product2);
+        productManager.displayProduct(0);
+        productManager.displayProduct(1);
+        productManager.displayProduct(2);
+        CompareTest compareTest = new CompareTest();
+//        Collections.sort(productManager,compareTest);
+    }
+}
